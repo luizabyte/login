ae4641f4210f2311173737d5f0ed122cb529496d
Maria Luiza Garcia <marialuiza.garcia05117@gmail.com>
2025-01-05 21:47:10 -0300

    Add files via upload

---
+++ log.js
@@ -0,0 +1,12 @@
+function logar(){
+    var login= document.getElementById('login').value;
+    var senha= document.getElementById('senha').value;
+
+    if(login == "admin" && senha== "admin"){
+        alert('Sucesso');
+        location.href="index.html"
+    }
+    else{
+        alert('Usuário ou senha incorretos');
+    }
+}
